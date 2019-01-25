21118a747b250d1358cc6f287b6b3b7558c24069
highmountaintea <jlpub@coolimage.com>
2019-01-25 15:40:53 -0500

    added more features

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function generateId() {
   return ('b' + Math.random()).replace('.', '');
 }
@@ -9,9 +11,31 @@ function injectCss(selectors, css) {
   document.getElementsByTagName('head')[0].appendChild(style);
 }
 
+function selectorsToString(selectors) {
+  if (selectors == null) return '';
+  if (typeof selectors === 'string') return selectors;
+  if (selectors instanceof Array) {
+    var arrayResult = [];
+    for (var i = 0; i < selectors.length; i++) {
+      arrayResult.push(selectorsToString(selectors[i]));
+    }
+    return arrayResult.join(',');
+  }
+  // assume an object
+  var objResult = [];
+  var spacer = selectors.spacer ? ' ' : '';
+  var prefix = selectors.prefix == null ? '' : '' + selectors.prefix + spacer;
+  var postfix = selectors.postfix == null ? '' : '' + spacer + selectors.postfix;
+  for (var j = 0; j < selectors.selectors.length; j++) {
+    var val = selectorsToString(selectors.selectors[i]);
+    objResult.push(prefix + val + postfix);
+  }
+  return objResult.join(',');
+}
+
 function brush(selectors, css) {
-  if (selectors == null) return generateId();
-  injectCss(selectors, css);
+  if (arguments.length == 0) return generateId();
+  injectCss(selectorsToString(selectors), css);
 }
 
 module.exports = brush;
